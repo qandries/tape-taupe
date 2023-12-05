712f6ae02425b24f2df3d5933842efc8c6f6d72e
q.andries <q.andries@emissic.com>
2023-12-05 11:37:46 +0100

    remove jquery

--- asset/js/correct.js
+++ asset/js/correct.js
@@ -1,3 +1,5 @@
+/** @format */
+
 var horizontalMax;
 var verticalMax;
 var removeTimer;
@@ -7,27 +9,51 @@ var score = 0;
 var apparitionTimer = 2000;
 var disappearanceTimer = 1800;
 
-$(document).ready(function() {
-    $('[data-use="gameOverPage"]').on('click', function() {
+function addEventListener(el, eventName, eventHandler, selector) {
+    if (selector) {
+        const wrappedHandler = (e) => {
+            if (!e.target) return;
+            const el = e.target.closest(selector);
+            if (el) {
+                eventHandler.call(el, e);
+            }
+        };
+        el.addEventListener(eventName, wrappedHandler);
+        return wrappedHandler;
+    } else {
+        const wrappedHandler = (e) => {
+            eventHandler.call(el, e);
+        };
+        el.addEventListener(eventName, wrappedHandler);
+        return wrappedHandler;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    addEventListener(document.querySelector('[data-use="gameOverPage"]'), "click", function () {
         //playSound('./assets/sound/background_sound.mp3');
-        $('[data-use="life"] span').html(life);
+        document.querySelector('[data-use="life"] span').innerHTML = life;
         renderGameboard(5, 5);
-        interval =  setInterval(displayTarget, apparitionTimer);
+        interval = setInterval(displayTarget, apparitionTimer);
     });
 
+    addEventListener(
+        document.getElementById("body"),
+        "click",
+        function () {
+            clearTimeout(removeTimer);
+            //playSound('./assets/sound/remove.mp3');
+            removeTarget("true");
+        },
+        "#target img"
+    );
 
-    $('body').on('click','#target img', function() {
-        clearTimeout(removeTimer);
-        //playSound('./assets/sound/remove.mp3');
-        removeTarget('true');
-    });
-
- /*   function playSound(sound) {
+    /*   function playSound(sound) {
         $('body').append('<audio src="' + sound + '" autoplay></audio>');
     }*/
     function incrementScore(increment) {
         score += increment;
-        $('[data-use="score"] span').html(score);
+        document.querySelector('[data-use="score"] span').innerHTML = score;
         apparitionTimer -= 100;
         disappearanceTimer = apparitionTimer;
         clearInterval(interval);
@@ -35,7 +61,7 @@ $(document).ready(function() {
     }
     // Display an alert containing Game Over
     function gameOver() {
-        $('section[data-use="gameboard"]').html('<h1>You loose</h1>');
+        document.querySelector('section[data-use="gameboard"]').innerHTML = "<h1>You loose</h1>";
         clearTimeout(removeTimer);
         clearInterval(interval);
         //alert('Game Over');
@@ -43,9 +69,9 @@ $(document).ready(function() {
     // Decrement the lifes, and display Game Over
     // if lifes is equal or below 0
     function loseLife() {
-        life --;
-        $('[data-use="life"] span').html(life);
-        if(life <= 0) {
+        life--;
+        document.querySelector('[data-use="life"] span').innerHTML = life;
+        if (life <= 0) {
             gameOver();
         }
     }
@@ -54,8 +80,8 @@ $(document).ready(function() {
     // If Status == true, increment score
     // Else execute loseLife
     function removeTarget(status) {
-        $('#target').remove();
-        if(status == 'true') {
+        document.querySelector("#target").remove();
+        if (status == "true") {
             incrementScore(1);
         } else {
             loseLife();
@@ -67,22 +93,31 @@ $(document).ready(function() {
     function displayTarget() {
         var hor = 1 + Math.floor(Math.random() * horizontalMax);
         var ver = 1 + Math.floor(Math.random() * verticalMax);
-        $('div[data-vertical="' + ver + '"] div[data-horizontal="' + hor + '"]').html('<div id="target" class="imgDiv"><img src="./asset/image/racoon.png"></div>');
-        removeTimer = setTimeout(function() {
-            removeTarget('false');
+        document.querySelector(
+            'div[data-vertical="' + ver + '"] div[data-horizontal="' + hor + '"]'
+        ).innerHTML = '<div id="target" class="imgDiv"><img src="./asset/image/racoon.png"></div>';
+        removeTimer = setTimeout(function () {
+            removeTarget("false");
         }, disappearanceTimer);
     }
     function renderGameboard(horizontal = 10, vertical = 10) {
         horizontalMax = horizontal;
         verticalMax = vertical;
         var gameBoard = "";
-        for (var i = 1; i <= vertical ; i++) {
+        for (var i = 1; i <= vertical; i++) {
             gameBoard += "<div data-vertical='" + i + "'>";
-            for (var j = 1; j <= horizontal ; j++) {
-                gameBoard += "<div style='height:" + 100 / vertical + "vh;width:" + 100 / horizontal + "%;' data-horizontal='" + j + "'></div>";
+            for (var j = 1; j <= horizontal; j++) {
+                gameBoard +=
+                    "<div style='height:" +
+                    100 / vertical +
+                    "vh;width:" +
+                    100 / horizontal +
+                    "%;' data-horizontal='" +
+                    j +
+                    "'></div>";
             }
             gameBoard += "</div>";
         }
-        $('section[data-use="gameboard"]').html(gameBoard);
+        document.querySelector('section[data-use="gameboard"]').innerHTML = gameBoard;
     }
 });
--- asset/js/tiptap.js
+++ asset/js/tiptap.js
@@ -1,3 +1,5 @@
+/** @format */
+
 let abscisseMax;
 let ordonneeMax;
 let removeTimer;
@@ -6,96 +8,152 @@ let score = 0;
 let life = 5;
 let appearanceTimer = 1500;
 let disappearanceTimer = 1200;
-$(document).ready(function () {
+
+function addEventListener(el, eventName, eventHandler, selector) {
+    if (selector) {
+        const wrappedHandler = (e) => {
+            if (!e.target) return;
+            const el = e.target.closest(selector);
+            if (el) {
+                eventHandler.call(el, e);
+            }
+        };
+        el.addEventListener(eventName, wrappedHandler);
+        return wrappedHandler;
+    } else {
+        const wrappedHandler = (e) => {
+            eventHandler.call(el, e);
+        };
+        el.addEventListener(eventName, wrappedHandler);
+        return wrappedHandler;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
     StartGame();
 
-    $('body').on('click', '#target img',function () {
-        clearTimeout(removeTimer);
-        //playSound('../sound/remove.mp3');
-        removeTarget(true);
-    });
+    addEventListener(
+        document.querySelector("body"),
+        "click",
+        function () {
+            clearTimeout(removeTimer);
+            //playSound('../sound/remove.mp3');
+            removeTarget(true);
+        },
+        "#target img"
+    );
 
     function StartGame() {
-        $('section[data-use="gameOverPage"]').html('<div id="start"><p>Do you want to start : <br></p><button id="button" type="button">Start</button></div>');
-        $('body').on('click', 'button', function () {
-            $('#start').remove();
-            score = 0;
-            life = 5;
-            //playSound('../sound/background_sound.mp3');
-            renderGameboard(5,5);
-            displayMenu(score,life);
-            interval = setInterval(displayTarget, appearanceTimer);
-        });
+        document.querySelector('section[data-use="gameOverPage"]').innerHTML =
+            '<div id="start"><p>Do you want to start : <br></p><button id="button" type="button">Start</button></div>';
+
+        addEventListener(
+            document.querySelector("body"),
+            "click",
+            function () {
+                document.getElementById("start").remove();
+                score = 0;
+                life = 5;
+                //playSound('../sound/background_sound.mp3');
+                renderGameboard(5, 5);
+                displayMenu(score, life);
+                interval = setInterval(displayTarget, appearanceTimer);
+            },
+            "button"
+        );
     }
 
-/*    function playSound(sound) {
+    /*    function playSound(sound) {
         $('body').append('<audio src="' + sound + '" autoplay></audio>');
     }
 */
     function displayMenu(score, life) {
-        $('section[data-use="menu"]').html('<div class="menu"><ul><li>Score : ' + score + '</li><li>Life : ' + life + '</li></ul></div>')
+        document.querySelector('section[data-use="menu"]').innerHTML =
+            '<div class="menu"><ul><li>Score : ' +
+            score +
+            "</li><li>Life : " +
+            life +
+            "</li></ul></div>";
     }
 
     function incrementScore(increment) {
         score = score + increment;
-        displayMenu(score,life);
+        displayMenu(score, life);
         //appearanceTimer -= 100;
         //disappearanceTimer -= 50;
         clearInterval(interval);
         interval = setInterval(displayTarget, appearanceTimer);
     }
 
-// Display the game over window and reinitialize the game
-   function gameOver(score) {
-       $('section[data-use="gameOverPage"]').html('<div id="gameOver"><p>Here\'s your score : ' + score + '<br> Do you want to continue : <br></p><button id="button" type="button">Restart</button></div>');
-       clearTimeout(removeTimer);
-       clearInterval(interval);
-       $('body').on('click', 'button', function () {
-           life = 3;
-           $('#gameOver').remove();
-       });
-   }
+    // Display the game over window and reinitialize the game
+    function gameOver(score) {
+        document.querySelector('section[data-use="gameOverPage"]').innerHTML =
+            '<div id="gameOver"><p>Here\'s your score : ' +
+            score +
+            '<br> Do you want to continue : <br></p><button id="button" type="button">Restart</button></div>';
+        clearTimeout(removeTimer);
+        clearInterval(interval);
+        addEventListener(
+            document.querySelector("body"),
+            "click",
+            function () {
+                life = 3;
+                document.getElementById("gameOver").remove();
+            },
+            "button"
+        );
+    }
 
-   function loseLife() {
-       life--;
-       displayMenu(score,life);
-       if(life <= 0) {
-          gameOver(score);
-       }
-   }
+    function loseLife() {
+        life--;
+        displayMenu(score, life);
+        if (life <= 0) {
+            gameOver(score);
+        }
+    }
 
-   // Allows the target to get removed either by clicking it (status exist then score goes up) or let it pass(status is false then lose a life)
-   function removeTarget(status) {
-      $('#target').remove();
-      if (status){
-         incrementScore(1);
-      }else{
-         loseLife();
-      }
-   }
+    // Allows the target to get removed either by clicking it (status exist then score goes up) or let it pass(status is false then lose a life)
+    function removeTarget(status) {
+        document.getElementById("target").remove();
+        if (status) {
+            incrementScore(1);
+        } else {
+            loseLife();
+        }
+    }
 
     // Generate pseudo-random position for the target.
     // Launch countdown to disappearance.
-   function displayTarget() {
-      let abs = 1 + Math.floor(Math.random() * abscisseMax);
-      let ord = 1 + Math.floor(Math.random() * ordonneeMax);
-      $('div[data-ordonnee="' + ord + '"] div[data-abscisse="' + abs + '"]').html('<div id="target" class="imgDiv"><img id="hit" src="./asset/image/racoon.png"></div>');
-      removeTimer = setTimeout(function () {
-         removeTarget(false)
-      }, disappearanceTimer)
-   }
+    function displayTarget() {
+        let abs = 1 + Math.floor(Math.random() * abscisseMax);
+        let ord = 1 + Math.floor(Math.random() * ordonneeMax);
+        document.querySelector(
+            'div[data-ordonnee="' + ord + '"] div[data-abscisse="' + abs + '"]'
+        ).innerHTML =
+            '<div id="target" class="imgDiv"><img id="hit" src="./asset/image/racoon.png"></div>';
+        removeTimer = setTimeout(function () {
+            removeTarget(false);
+        }, disappearanceTimer);
+    }
 
-   function renderGameboard(abscisse = 10, ordonnee = 10) {
-       abscisseMax = abscisse;
-       ordonneeMax = ordonnee;
-       let gameboard = "";
-       for (let i=1; i <= ordonnee; i++){
-          gameboard += "<div data-ordonnee='" + i + "'>";
-          for (let j=1; j <= abscisse; j++){
-             gameboard += "<div style='height:" + 100 / ordonnee + "vh;width: " + 100 / abscisse + "%;' data-abscisse='" + j + "'></div>"
-          }
-          gameboard += "</div>";
-       }
-       $('section[data-use="gameboard"]').html(gameboard);
-   }
+    function renderGameboard(abscisse = 10, ordonnee = 10) {
+        abscisseMax = abscisse;
+        ordonneeMax = ordonnee;
+        let gameboard = "";
+        for (let i = 1; i <= ordonnee; i++) {
+            gameboard += "<div data-ordonnee='" + i + "'>";
+            for (let j = 1; j <= abscisse; j++) {
+                gameboard +=
+                    "<div style='height:" +
+                    100 / ordonnee +
+                    "vh;width: " +
+                    100 / abscisse +
+                    "%;' data-abscisse='" +
+                    j +
+                    "'></div>";
+            }
+            gameboard += "</div>";
+        }
+        document.querySelector('section[data-use="gameboard"]').innerHTML = gameboard;
+    }
 });
